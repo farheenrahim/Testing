220504b627c532b720326b93beb1658806a4cc52
Farheen Rahim <farheen.rahim1@gmail.com>
2017-03-23 17:33:49 -0400

    Selector

---
+++ Selenium/Selenium2/Selenium2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        string url = "http://testing.todvachev.com/selectors/id/";
+        string id = "testImage";
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+        IWebElement element = driver.FindElement(By.Id(id));
+        if(element.Displayed)
+        {
+            Greenmessage("Yes! I can see the element");
+                   }
+        else
+        {
+            Redmessage("Well something is wrong. I coudnt see the element");
+                    }
+        driver.Quit();
+        }
+
+    private static void Redmessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void Greenmessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
---
+++ Selenium/selenium3/selenium3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string url = "http://testing.todvachev.com/selectors/class-name/";
+        string classname = "testClass";
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+        IWebElement element = driver.FindElement(By.ClassName(classname));
+        Console.WriteLine(element.Text);
+        driver.Quit();
+        Console.ReadKey();
+    }
+   
+  
+}
+
